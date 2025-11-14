c2f46c6346c2b20626153be7633d4655a699f699
Atul Goyal <atul250603@gmail.com>
2025-11-14 17:37:44 +0530

    Update Work.tsx

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -5,13 +5,22 @@ function Work(){
     const projects:projectType[]=[
     {
       id: 1,
+      name: "Iconhub",
+      description: "A clean, powerful icon customizer—browse thousands of icons, tweak every detail, and export in one click.",
+      skills: ["Nextjs", "Dexie.js", "Shadcn", "Typescript", "Tailwind CSS"],
+      liveLink: "https://icon-hub.vercel.app/",
+      githubLink: "https://github.com/atul250603/Iconhub"
+    }, 
+    {
+      id: 2,
       name: "Quizzify",
      description: "Enter any topic and get instant, personalized quizzes to enhance your learning. Receive detailed analytics and curated resources to fill knowledge gaps.",
      skills: ["Nextjs", "Hono", "Cloudflare Worker", "Cloudflare KV", "Postgres", "Gemini", "Tailwind CSS"],
-     liveLink: "https://quizzify.site"
+     liveLink: "https://quizzify.site",
+      githubLink: "https://github.com/atul250603/quizzify"
     }, 
     {
-        id:2,
+        id:3,
         name:"Toastrr",
         description:"It's a cool & lightweight toast notification library for React.js and Next.js",
         skills:["React","Javascript","CSS", "npm", "Singleton Pattern"],
@@ -19,28 +28,28 @@ function Work(){
         githubLink:"https://github.com/atul250603/toastrr"
     },
     {
-        id:3,
+        id:4,
         name:"TalentX",
         description:"This platform solves the problems of recruiters and students. Students can find people to collaborate on their projects, people can organize hackathons, and recruiters can post job openings, conduct assignments, and take interviews.",
         skills:["React","Node","Express","Socket.IO","WebRTC","Firebase","Tailwind CSS","MongoDB"],
         liveLink:"https://talenthunt-seven.vercel.app/",
         githubLink:"https://github.com/atul250603/talenthunt"
     },{
-        id:4,
+        id:5,
         name:"Paybuddy",
         description:"This is a platform where you can keep track of all your transactions like how much money you owe someone and vice-versa.",
         skills:["React","Node","Express","MongoDB","Bootstrap"],
         liveLink:"https://paybuiddy-backend-q2a6.vercel.app/",
         githubLink:"https://github.com/atul250603/paybuddy_fe"
     },{
-        id:5,
+        id:6,
         name:"Opino",
         description:"A user-friendly platform enabling quiz creation and participation.Implemented features for creating new quizzes, updating new quizzes, attempting an existing quiz, and generating results.",
         skills:["React","Node","Express","MongoDB","Tailwind CSS"],
         liveLink:"https://opino-fe.vercel.app/",
         githubLink:"https://github.com/Atul250603/opino_fe"
     },{
-        id:6,
+        id:7,
         name:"Tasklist",
         description:"This a platform where you can list down all your tasks, check the status of them, change their status to completed, and delete them as well.",
         skills:["React","Node","Express","MongoDB","Bootstrap","Auth0"],
